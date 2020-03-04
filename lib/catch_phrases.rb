fbdc4e1c667d77bfb5e45320e492e32776982b0b
Mina Malaj <mina.malaj@hotmail.com>
2020-03-04 14:37:59 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,6 @@
-def mario
-    status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+ $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+ 
+ def mario
     phrase = "It's-a me, Mario!"
   puts phrase
 end
